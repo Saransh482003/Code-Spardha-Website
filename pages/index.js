10542f25ea0ea474f6056146cd7a2166e87efd49
Saransh <saini.saransh03@gmail.com>
2023-10-23 17:49:45 +0530

    Home done

--- pages/index.js
+++ pages/index.js
@@ -79,6 +79,47 @@ const home = () => {
           
         </ol>
       </section>
+      <section className={styler.howTo}>
+        <h1 className={styler.howHeader}>Organiser Team</h1>
+        <div className={styler.container}>
+          <h1 className={styler.contHead}>Faculty coordinators</h1>
+          <div className={styler.entry}>
+            <h3 className={styler.name}>Dr. Mamta Mittal</h3>
+            <p className={styler.desc}>HOD B.Sc. Data Analytics</p>
+          </div>
+        </div>
+        <div className={styler.container}>
+          <h1 className={styler.contHead}>Coordinators</h1>
+          <div className={styler.entry}>
+            <h3 className={styler.name}>Saransh Saini</h3>
+            <p className={styler.desc}>3rd Year B.Sc. Data Analytics Student</p>
+            <p className={styler.desc}>8178708402</p>
+          </div>
+          <div className={styler.entry}>
+            <h3 className={styler.name}>Mamta</h3>
+            <p className={styler.desc}>3rd Year B.Sc. Data Analytics Student</p>
+            <p className={styler.desc}>8800147938</p>
+          </div>
+        </div>
+        <div className={styler.container}>
+          <h1 className={styler.contHead}>Co-coordinators</h1>
+          <div className={styler.entry}>
+            <h3 className={styler.name}>Sonu</h3>
+            <p className={styler.desc}>2rd Year B.Sc. Data Analytics Student</p>
+            <p className={styler.desc}>8178944525</p>
+          </div>
+          <div className={styler.entry}>
+            <h3 className={styler.name}>Hamid</h3>
+            <p className={styler.desc}>2rd Year B.Sc. Data Analytics Student</p>
+            <p className={styler.desc}>9576014911</p>
+          </div>
+          <div className={styler.entry}>
+            <h3 className={styler.name}>Suryansh</h3>
+            <p className={styler.desc}>2rd Year B.Sc. Data Analytics Student</p>
+            <p className={styler.desc}>9650972122</p>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
