bd0b3e445c409f89a5f9a28c75f7abcc1f496341
Saransh <saini.saransh03@gmail.com>
2023-10-24 21:55:01 +0530

    Lively Done a bit

--- pages/index.js
+++ pages/index.js
@@ -20,15 +20,19 @@ const home = () => {
       <section className={styler.howTo}>
         <h1 className={styler.howHeader}>How To Register?</h1>
         <div className={styler.steps}>
-          <Link href="https://docs.google.com/forms/d/e/1FAIpQLScuGJhP38w_iIG8il3smlOG4bx4MD8Cp1d60-Xh5nlgmrydNQ/viewform?usp=sf_link" className={styler.card}>
+          <Link href="https://docs.google.com/forms/d/e/1FAIpQLScuGJhP38w_iIG8il3smlOG4bx4MD8Cp1d60-Xh5nlgmrydNQ/viewform?usp=sf_link" target='_blank' className={styler.card}>
+            <div className={styler.imgCont}>
             <Image src="/Home/Howto/codespardha.png" className={styler.img} width={1500} height={1500}></Image>
+            </div>
             <div className={styler.stepContent}>
               <h3 className={styler.stepHead}>Step 1 : Fill in the Registeration Form</h3>
               <p className={styler.stepDesc}>Welcome to Code Spardha! We invite participants from all backgrounds to take part in this event. To begin your journey, please complete the Registration Form with utmost accuracy and sincerity. Your provided information will be used to generate your certificates, making this step essential. Ensure all fields are filled out seriously and comprehensively. Only registered participants will be eligible for certificates.</p>
             </div>
           </Link>
           <Link href="https://www.hackerrank.com/code-spardha-arc-3" target='_blank' className={styler.card}>
+          <div className={styler.imgCont}>
             <Image src="/Home/Howto/hacker.png" className={styler.img} width={1500} height={1500}></Image>
+          </div>
             <div className={styler.stepContent}>
               <h3 className={styler.stepHead}>Step 2 : Sign Up on Hackerrank</h3>
               <p className={styler.stepDesc}>Visit the HackerRank website, sign up, and provide accurate information. Use a valid email address. After creating your account, sign in, and navigate to the competition page. Click 'Sign Up' before the deadline on November 3rd, 11:59 PM. Your active participation is crucial. Thank you for your cooperation.</p>
