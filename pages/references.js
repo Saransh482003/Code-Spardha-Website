66cd340c8f24953401c05d758b19eaf0670aba1a
Saransh <saini.saransh03@gmail.com>
2023-11-03 16:21:15 +0530

    References added

--- pages/references.js
+++ pages/references.js
@@ -21,9 +21,9 @@ const references = () => {
       <div className={styler.ques}>
         <h3 className={styler.quesName}>Level 3 : The Traveller's Economy</h3>
         <h4 className={styler.quesPart} style={{marginTop:"1rem"}}>Sample Input 0</h4>
-        <Image src="/Home/reference/traveler 1.png" style={{width:"50%",height:"50%"}} className={styler.img} width={1500} height={1500}></Image>
+        <Image src="/Home/reference/traveler1.png" style={{width:"50%",height:"50%"}} className={styler.img} width={1500} height={1500}></Image>
         <h4 className={styler.quesPart} style={{marginTop:"1rem"}}>Sample Input 1</h4>
-        <Image src="/Home/reference/traveler 2.png" className={styler.img} width={1500} height={1500}></Image>
+        <Image src="/Home/reference/traveler2.png" className={styler.img} width={1500} height={1500}></Image>
       </div>
     </section>
   )
